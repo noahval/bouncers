63f54172036dfdf21daec570bccfd22096a80b79
noah <noahvalyi@gmail.com>
2020-02-04 22:09:23 -0800

    ssss

--- Bouncers/bouncers/Assets/BallController.cs
+++ Bouncers/bouncers/Assets/BallController.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public Rigidbody rb;
+    float x;
+    float y;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        x = Random.Range(-3.0f, 3.0f);
+        y = Random.Range(-3.0f, 3.0f);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        rb.AddForce(new Vector3(x, y) * 10f);
     }
 
     void OnMouseDown()
